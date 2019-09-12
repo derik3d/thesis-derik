3ab5532cbe764d9d9fd0d8441add4fe85c5c673b
pina <pina@DESKTOP-RKA6SA2>
2019-09-12 08:48:34 -0500

    mapping and handling kegg request
    mapping for restkegg with error, now sends 400
    all* exceptions handled for communication with kegg

--- appthesis/src/main/java/com/thesisderik/appthesis/RestController.java
+++ appthesis/src/main/java/com/thesisderik/appthesis/RestController.java
@@ -1,6 +1,8 @@
 package com.thesisderik.appthesis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,8 +28,19 @@ public class RestController {
 	
 	@RequestMapping("kegg2pubchem/")
 	@ResponseBody
-	public ResponseKegg kegg2pubchem(@RequestParam(value="id", defaultValue="") String id) {
-		return idCrawlerService.fromKeggIdGetPubchem(id);
+	public ResponseEntity kegg2pubchem(@RequestParam(value="id", defaultValue="") String id) {
+		
+		ResponseKegg res = idCrawlerService.fromKeggIdGetPubchem(id);
+		
+		if (res == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	    }
+		
+		return new ResponseEntity<>(res,HttpStatus.OK);
+		
+		//ResponseKegg res = new ResponseKegg();
+		//res.setCpd(id);
+		//return res;
 	}
 	
 	@RequestMapping("bigg2kegg/")
--- appthesis/src/main/java/idscrawler/IdCrawlerService.java
+++ appthesis/src/main/java/idscrawler/IdCrawlerService.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import idscrawler.entities.ResponseKegg;
@@ -19,9 +20,48 @@ public class IdCrawlerService {
 	
 	public ResponseKegg fromKeggIdGetPubchem(String id) {
 		
-		ResponseEntity resp = restTemplate.getForEntity( kegg2PubchemUrl + id, ResponseKegg.class);
+		try {
+			ResponseEntity<String> resp = restTemplate.getForEntity( kegg2PubchemUrl + id, String.class);		
+
+			if(resp.getStatusCode() == HttpStatus.OK) {
+				
+				try {
+					ResponseKegg res = new ResponseKegg();
+					
+					String resString = resp.getBody();
+
+					if(resString.length() < 1)
+						throw new Exception("zero length data");
+					
+					String cpd;
+					String pubchem;
+					if(resString.contains("\t") && resString.contains("\n") && resString.contains("pubchem") && resString.contains("cpd")) {
+
+						cpd = resString.substring(0,resString.indexOf("\t")).replace("cpd:","");
+						pubchem = resString.substring(resString.indexOf("\t")+1, resString.indexOf("\n")).replace("pubchem:", "");
+						
+						if(cpd.length() < 1 || pubchem.length() < 1)
+							throw new Exception("zero length data");
+						
+						res.setCpd(cpd);
+						res.setPubchem(pubchem);
+						return res;
+						
+					}
+				
+				}catch(Exception ex) {
+					System.out.println("not reading well kegg response");
+					//ex.printStackTrace();
+					return null;
+				}
+			}
+		} catch (RestClientException e) {
+			System.out.println("400 from kegg");
+			return null;
+			//e.printStackTrace();
+		}
 		
-		return resp.getStatusCode() == HttpStatus.OK ? (ResponseKegg)resp.getBody() : null;
+		return null;
 		
 	}
 	
