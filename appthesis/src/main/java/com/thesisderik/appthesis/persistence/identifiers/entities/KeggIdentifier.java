288e2bcd88f740e5f88c0472df5a2e5e778d8829
MAC AIR <derik3d@gmail.com>
2019-09-24 00:39:36 -0500

    basic entities for ids

---
+++ appthesis/src/main/java/com/thesisderik/appthesis/persistence/identifiers/entities/KeggIdentifier.java
@@ -0,0 +1,23 @@
+package com.thesisderik.appthesis.persistence.identifiers.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(
+	name = "KEGG_IDENTIFIERS"
+)
+public class KeggIdentifier {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "NAME", unique = true, nullable = false, length = 100)
+	private String name;
+	
+}
