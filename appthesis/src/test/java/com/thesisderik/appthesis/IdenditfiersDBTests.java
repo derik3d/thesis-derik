94786b99bb781f318fe3c1bcbcfdb011daf9b335
MAC AIR <derik3d@gmail.com>
2019-09-26 23:32:50 -0500

    tests for ident db interface

--- appthesis/src/test/java/com/thesisderik/appthesis/IdenditfiersDBTests.java
+++ appthesis/src/test/java/com/thesisderik/appthesis/IdenditfiersDBTests.java
@@ -2,6 +2,7 @@ package com.thesisderik.appthesis;
 
 
 import org.junit.Test;
+import org.junit.experimental.results.ResultMatchers;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,21 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.thesisderik.appthesis.persistence.identifiers.entities.PubchemIdentifier;
+import com.thesisderik.appthesis.services.INamesIdentifiersService;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,8 +35,12 @@ public class IdenditfiersDBTests {
 
 	@Autowired
 	private MockMvc mvc;
+	
+	@Autowired
+	private INamesIdentifiersService iNamesIdentifiersService;
 
 	@Test
+	@Order(1)
 	public void test_Hello() throws Exception {
 		this.mvc.perform(get("/identifierspersist/")).andExpect(status().isOk()).andExpect(content().string("hola"));
 	}
@@ -33,11 +48,49 @@ public class IdenditfiersDBTests {
 	
 
 	@Test
-	@Order(1)
 	public void test_save_success() throws Exception {
 		this.mvc.perform(MockMvcRequestBuilders.post("/identifierspersist/persist/pubchem/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"idpubchem\"}")).andExpect(status().isOk()).andExpect(content().json("{\"name\":\"idpubchem\"}"));
+                .content("{\"name\":\"idtest1\"}")).andExpect(status().isOk()).andExpect(content().json("{\"name\":\"idtest1\"}"));
+		
+	}
+	
+
+	@Test
+	public void test_save_consult() throws Exception {
+		this.mvc.perform(MockMvcRequestBuilders.post("/identifierspersist/persist/pubchem/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"idtest2\"}")).andExpect(status().isOk()).andExpect(content().json("{\"name\":\"idtest2\"}"));
+		
+		
+		this.mvc.perform(get("/identifierspersist/getByName/pubchem/?id=idtest2")).andExpect(status().isOk()).andExpect(content().json("{\"name\":\"idtest2\"}"));
+
+	}
+	
+
+	
+
+	@Test
+	public void test_interface_save_consult() throws Exception {
+		
+		iNamesIdentifiersService.saveIdentifier(new PubchemIdentifier("idtest3"));
+		Optional<PubchemIdentifier> pubchemIdentifierByName = iNamesIdentifiersService.getPubchemIdentifierByName("idtest3");
+		assertTrue(pubchemIdentifierByName.isPresent());
+		
+
+	}
+	
+	
+	
+	/*
+
+	@Test
+	@Order(4)
+	public void test_save_success3() throws Exception {
+		ResultActions perform = this.mvc.perform(MockMvcRequestBuilders.post("/identifierspersist/persist/pubchem/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"idpubchem\"}"));
+		System.out.println(perform.toString());
 		
 	}
 	
