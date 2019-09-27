4e1d397d5b05c4964a1b10ac178c97dd84b99515
MAC AIR <derik3d@gmail.com>
2019-09-27 01:27:46 -0500

    controller built for identifiersmanager

--- appthesis/src/main/java/com/thesisderik/appthesis/controllers/IdentifiersManagerController.java
+++ appthesis/src/main/java/com/thesisderik/appthesis/controllers/IdentifiersManagerController.java
@@ -1,9 +1,16 @@
 package com.thesisderik.appthesis.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.thesisderik.appthesis.persistence.identifiers.entities.KgmlIdentifier;
+import com.thesisderik.appthesis.persistence.identifiers.entities.PubchemIdentifier;
 import com.thesisderik.appthesis.services.INamesIntegrator;
 
 @Controller
@@ -13,7 +20,31 @@ public class IdentifiersManagerController {
 	@Autowired
 	INamesIntegrator iNamesIntegrator;
 	
+
+	@RequestMapping("process/kgml/")
+	public ResponseEntity<PubchemIdentifier> getKgmlByName(@RequestParam(value="id", defaultValue="") String name) {
+		
+		PubchemIdentifier res = iNamesIntegrator.processKgmlIdentifier(name).get();
+		
+		if (res == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	    }
+		
+		return new ResponseEntity<>(res,HttpStatus.OK);
+	}
 	
+
+	@RequestMapping("process/sbml/")
+	public ResponseEntity<PubchemIdentifier> getSbmlByName(@RequestParam(value="id", defaultValue="") String name) {
+		
+		PubchemIdentifier res = iNamesIntegrator.processSbmlIdentifier(name).get();
+		
+		if (res == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	    }
+		
+		return new ResponseEntity<>(res,HttpStatus.OK);
+	}
 	
 
 }
