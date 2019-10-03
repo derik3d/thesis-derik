9983cc96330b77137a7db6671ea2fb5ce442a5d6
MAC AIR <derik3d@gmail.com>
2019-10-03 15:01:25 -0500

    improved experiments entity

--- appthesis/src/main/java/com/thesisderik/appthesis/persistence/simplegraph/entities/PlainExperiment.java
+++ appthesis/src/main/java/com/thesisderik/appthesis/persistence/simplegraph/entities/PlainExperiment.java
@@ -5,27 +5,37 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table( name = "GRAPH_EXPERIMENTS" )
-public class Experiment {
+public class PlainExperiment {
 	
 	@Id
+	private Long id;
+	
+	@Column(name = "TITLE",  nullable = false, unique = true)
 	private String title;
 	
 	@Column(name = "DESCRIPTION",  nullable = false)
 	private String description;
 
-	@OneToMany
+	@ManyToMany
 	private Set<PlainGroup> groups;
 	
-	@OneToMany
+	@ManyToMany
 	private Set<PlainFeature> features;
 	
+	@ManyToOne
+	private PlainTask task;
 	
+	@Column(name = "TASK_DESCRIPTION_COMMAND",  nullable = false)
+	private String taskDescriptionCommand;
 	
-	
+	@Column(name = "FEATURE_NAME_OVERRIDE",  nullable = false)
+	private String featureNameOverride;
 
 }
